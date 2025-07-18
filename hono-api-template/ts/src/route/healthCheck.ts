8351482558ea8134882815b9ba32bd63e67650a8
Kojima Kohei <125636684+k-kojima-yumemi@users.noreply.github.com>
2025-07-18 14:10:02 +0900

    Add `/health` endpoint

--- hono-api-template/ts/src/route/healthCheck.ts
+++ hono-api-template/ts/src/route/healthCheck.ts
@@ -1,7 +1,11 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
-import { healthCheckRoute } from "../schema/hono-api-template/healthCheck";
+import { healthCheckRoute, healthRoute } from "../schema/hono-api-template/healthCheck";
 
 export const HealthCheckApp = new OpenAPIHono();
 HealthCheckApp.openapi(healthCheckRoute, (c) => {
+    return c.json({ status: "ok" } as const);
+});
+
+HealthCheckApp.openapi(healthRoute, (c) => {
     return c.json({ status: "ok" } as const);
 });
